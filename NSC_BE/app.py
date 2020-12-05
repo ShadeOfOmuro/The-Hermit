ff2b5bb1fc353479fb92c9e170579b9744108ff4
Shironi-dev <61903023+Shironi-dev@users.noreply.github.com>
2020-12-05 11:57:37 +0700

    Add files via upload

--- NSC_BE/app.py
+++ NSC_BE/app.py
@@ -2,18 +2,38 @@ from flask import *
 app = Flask(__name__)
 class backpack :
     question = ["Nan","1" , "2" , "3"  , "4"  , "5"  , "6"  , "7"  , "8"  , "9"  , "10"]
-    curr_question = 1;
+    curr_question = 1
     score = 0
+    Name = ""
+    age = 0
+    sex = 0
+    # for sex 
+    # 0 = male
+    # 1 = female 
 @app.route("/")
 def render_main() :
     backpack.curr_question = 1
     backpack.score = 0
+    backpack.name = ""
+    backpack.age = 0
+    backpack.sex = 0
     return render_template("index.html")
 
 @app.route("/info") 
 def render_info() :
     return render_template("info.html")
-
+@app.route("/registeration")
+def render_register() :
+    return render_template("register.html")
+@app.route("/formhandler" , methods=["POST"])
+def do_sth_with_data() :
+    backpack.age = request.form.get("age")
+    backpack.name = request.form.get("name")
+    backpack.sex = request.form.get("sex")
+    print(backpack.age,"\n")
+    print(backpack.name,"\n")
+    print(backpack.sex,"\n")
+    return redirect("/choicequiz")
 @app.route("/choicequiz")
 def render_quiz() :
     return render_template("quiz.html" , question = backpack.question[backpack.curr_question])
@@ -35,4 +55,5 @@ def render_mindwave() :
 @app.route("/full_result")
 def render_test_result() :
     return render_template("result.html")
+
 app.run(debug=True)
