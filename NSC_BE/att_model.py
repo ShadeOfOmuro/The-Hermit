44eeba6c63c360ac976cccebd2f54f487b42fa2f
Shironi-dev <61903023+Shironi-dev@users.noreply.github.com>
2021-01-08 21:32:36 +0700

    Beta flight #1

--- NSC_BE/att_model.py
+++ NSC_BE/att_model.py
@@ -1,5 +1,5 @@
 import pickle
-filename = 'NSC_BE/model_dump_save/att_model.sav'
+filename = 'model_dump_save/att_model.sav'
 knn = pickle.load(open(filename, 'rb'))
 def predict(data) :
     return knn.predict(data)[0]
